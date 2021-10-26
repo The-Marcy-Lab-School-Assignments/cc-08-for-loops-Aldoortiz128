60c25d35a42ffe83f7f06700155d81845fb5f078
aldoortiz128 <aldoortiz128@gmail.com>
2021-10-26 14:28:00 +0000

    Challenges completed

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 //1
 function countToTwenty(){
-  
+    
+for (let i = 0; i <= 20; i++) {
+ console.log (i);
+}
 }
 
 //2
 function countFromTwenty(){
-  
+    
+for (let i = 20; i <= 1; i--) {
+  console.log (i);
+}
 }
 
 //3
-function countToN(){
-  
+function countToN(N){
+for (let i = 0; i <= N; i++) {
+    console.log (i);
+}
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(N){
+for (let i = N; i >= 1; i--) {
+    console.log (i);
+}
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(N){
+for (let i = 1; i <= N; i++) {
+    if (i % 2) {console.log (i)}
 }
+}
+
+
+console.log(countToTwenty());
+console.log(countFromTwenty());
+console.log(countToN(50));
+console.log(countFromN(90));
+console.log(countEveryOdd(100));
